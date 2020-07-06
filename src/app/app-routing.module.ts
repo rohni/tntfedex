f4b985395500c824082b405bc481a13c50107747
Rohan Nicholls <rohan.nicholls@gmail.com>
2020-07-06 12:45:06 +0200

    routing for various pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { SignupSuccessComponent } from './signup-success/signup-success.component';
+import { SignupErrorComponent } from './signup-error/signup-error.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup-form', component: SignupFormComponent },
+  { path: '', redirectTo: '/signup-form', pathMatch: 'full' },
+  { path: 'signup-success', component: SignupSuccessComponent },
+  { path: 'signup-error', component: SignupErrorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
