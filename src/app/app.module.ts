32cd858d78eff47d6f44642a3e8b8d609516596b
Rohan Nicholls <rohan.nicholls@gmail.com>
2020-07-06 12:45:23 +0200

    dependencies etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { FormInputComponent } from './signup-form/input/input.component';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { SignupSuccessComponent } from './signup-success/signup-success.component';
+import { SignupErrorComponent } from './signup-error/signup-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent,
+    FormInputComponent,
+    HeaderComponent,
+    SignupSuccessComponent,
+    SignupErrorComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpClient],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
